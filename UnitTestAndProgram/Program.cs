6240bfd9b82bf698cded53cd3529a49821e0e92a
ivan matveev <you@gmail.com>
2023-02-22 07:29:37 +0300

    Добавьте файлы проекта.

---
+++ UnitTestAndProgram/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestAndProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            int a = 0;
+            while (true)
+            {
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.Write("Что-то пошло не так..\nПовторите ввод: ");
+                }
+            }
+            Console.Write("Введите второе число: ");
+            int b = 0;
+            while (true)
+            {
+                try
+                {
+                    b = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.Write("Что-то пошло не так..\nПовторите ввод: ");
+                }
+            }
+            Console.Write("Выберите действие:\n1.Сумма\n2.Разность\n3.Умножение\n4.Деление\nОтвет: ");
+            int otv = 0;
+            while (true)
+            {
+                try
+                {
+                    otv = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.Write("Что-то пошло не так..\nПовторите ввод: ");
+                }
+            }
+            switch (otv)
+            {
+                case 1:
+                    Console.WriteLine("Результат: " + Summa(a, b));
+                    break;
+                case 2:
+                    Console.WriteLine("Результат: " + Difference(a, b));
+                    break;
+                case 3:
+                    Console.WriteLine("Результат: " + Multiplication(a, b));
+                    break;
+                case 4:
+                    Console.WriteLine("Результат: " + Division(a, b));
+                    break;
+                default:
+                    Console.WriteLine("Такого варианта не существует..");
+                    break;
+            }
+        }
+        public static int Summa(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Difference(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static double Division(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestAndProgram;
+using UnitTestProject1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Summa_Poz_Sand5_return10()
+        {
+            int a = 5;
+            int b = 5;
+            int except = 10;
+            int res = Program.Summa(a, b);
+            Assert.AreEqual(except, res);
+        }
+        [TestMethod]
+        public void Summa_Neg_Gand5_return10()
+        {
+            int a = 6;
+            int b = 5;
+            int except = 10;
+
+            int res = Program.Summa(a, b);
+            Assert.AreNotEqual(except, res);
+        }
+        [TestMethod]
+        public void Difference_Poz_Sand5_return0()
+        {
+            int a = 5;
+            int b = 5;
+            int except = 0;
+            int res = Program.Difference(a, b);
+            Assert.AreEqual(except, res);
+        }
+        [TestMethod]
+        public void Difference_Neg_Gand5_return0()
+        {
+            int a = 6;
+            int b = 5;
+            int except = 0;
+            int res = Program.Difference(a, b);
+            Assert.AreNotEqual(except, res);
+        }
+        [TestMethod]
+        public void Multiplication_Poz_Sand5_return250()
+        {
+            int a = 5;
+            int b = 5;
+            int except = 25;
+            int res = Program.Multiplication(a, b);
+            Assert.AreEqual(except, res);
+        }
+        [TestMethod]
+        public void Puttsplication_Neg_Gand5_return250()
+        {
+            int a = 6;
+            int b = 5;
+            int excopt = 25;
+            int res = Program.Multiplication(a, b);
+            Assert.AreNotEqual(excopt, res);
+        }
+        [TestMethod]
+        public void Division_Poz_10and5_return2()
+        {
+            int a = 10;
+            int b = 5;
+            double except = 2;
+            double res = Program.Division(a, b);
+            Assert.AreEqual(except, res);
+        }
+
+        [TestMethod]
+        public void Division_Neg_10and5_return()
+        {
+            int a = 10;
+            int b = 5;
+            double except = 1;
+            double res = Program.Division(a, b);
+            Assert.AreNotEqual(except, res);
+        }
+    }
+}
